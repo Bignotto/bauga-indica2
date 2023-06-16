f3ceb07e212e99d0c5bd53087ab539b51b870cc7
Thiago Bignotto <bignotto@gmail.com>
2023-06-16 06:26:04 -0300

    chakra ui and theme global font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/components/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { Suwannaphum } from "next/font/google";
+import { theme } from "../styles/theme";
+
+const textFont = Suwannaphum({ subsets: ["latin"], weight: "400" });
+const headingFont = Suwannaphum({ subsets: ["latin"], weight: "700" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --font-suwannaphum: ${textFont.style.fontFamily};
+            --font-headingFont: ${headingFont.style.fontFamily};
+          }
+        `}
+      </style>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
