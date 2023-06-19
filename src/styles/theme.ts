989ed1e56e561f8f5265c285bab83423e43c3abb
Thiago Bignotto <bignotto@gmail.com>
2023-06-19 06:09:12 -0300

    fixed body font bold

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,7 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
   fonts: {
-    heading: "var(--font-suwannaphum)",
-    body: "var(--font-headingFont)",
+    body: "var(--font-suwannaphum)",
+    heading: "var(--font-headingFont)",
   },
 });
