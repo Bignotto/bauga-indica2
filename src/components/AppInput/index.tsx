3a39a3936c78818e3741fb091127541736d91674
Thiago Bignotto <bignotto@gmail.com>
2023-06-25 18:49:04 -0300

    added react hook form and yup and configured it

--- src/components/AppInput/index.tsx
+++ src/components/AppInput/index.tsx
@@ -8,8 +8,18 @@ type AppInputProps = InputProps & {
 export default function AppInput({ error, label, ...rest }: AppInputProps) {
   return (
     <Stack w="100%">
-      <Text fontSize={"sm"}>{label}:</Text>
-      <Input borderColor={error ? "red.500" : "blue.400"} {...rest} />
+      <Text fontSize={"sm"} mb="-1.5">
+        {label}:
+      </Text>
+      <Input
+        isInvalid={!!error}
+        errorBorderColor="crimson"
+        borderColor={"gray.800"}
+        _hover={{
+          borderColor: "blue.400",
+        }}
+        {...rest}
+      />
     </Stack>
   );
 }
--- src/pages/users/profile.tsx
+++ src/pages/users/profile.tsx
@@ -2,10 +2,36 @@ import AppInput from "@/components/AppInput";
 import AppLogo from "@/components/AppLogo";
 import { api } from "@/services/api";
 import { Avatar, Flex, Heading, Spinner, Stack, Text } from "@chakra-ui/react";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { User } from "@prisma/client";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+
+import * as yup from "yup";
+
+type FormDataProps = {
+  name: string;
+  email: string;
+  phone: string;
+};
+
+const profileSchema = yup.object({
+  name: yup.string().required("Seu nome é obrigatório."),
+  email: yup
+    .string()
+    .email("Não parece ser um endereço de e-mail válido.")
+    .required("Um endereço de e-mail é obrigatório."),
+  phone: yup
+    .string()
+    .required()
+    .test(
+      "len",
+      "Não parece ser um número de telefone válido.",
+      (val) => val.length === 11
+    ),
+});
 
 export default function Profile() {
   const router = useRouter();
@@ -13,13 +39,27 @@ export default function Profile() {
   const [userProfile, setUserProfile] = useState<User>();
   const [isLoading, setIsLoading] = useState(true);
 
+  const { control, handleSubmit, formState, setValue } = useForm<FormDataProps>(
+    {
+      resolver: yupResolver(profileSchema),
+    }
+  );
+
   //TODO: encapsulate this in a hook
   useEffect(() => {
     async function loadUserProfile() {
-      const response = await api.get(`users/${session?.user?.email}`);
-      setUserProfile(response.data);
-      console.log("loaded user again");
-      setIsLoading(false);
+      try {
+        const response = await api.get(`users/${session?.user?.email}`);
+        setUserProfile(response.data);
+        setValue("name", response.data.name);
+        setValue("email", response.data.email);
+        setValue("phone", response.data.phone);
+        console.log("loaded user again");
+      } catch (error) {
+        console.log({ error });
+      } finally {
+        setIsLoading(false);
+      }
     }
 
     if (status === "authenticated") loadUserProfile();
@@ -50,11 +90,50 @@ export default function Profile() {
               Perfil público de
             </Text>
             <Heading>{userProfile?.name}</Heading>
-            <AppInput
-              placeholder="Meu input"
-              label="Nome completo"
-              error={"Nome não pode estar em branco"}
-            />
+            <Stack w="full" mt="4">
+              <Controller
+                control={control}
+                name="name"
+                defaultValue=""
+                render={({ field: { onChange, value } }) => (
+                  <AppInput
+                    placeholder="Seu nome"
+                    label="Nome completo"
+                    error={null}
+                    value={value}
+                    onChange={onChange}
+                  />
+                )}
+              />
+              <Controller
+                control={control}
+                name="email"
+                defaultValue=""
+                render={({ field: { onChange, value } }) => (
+                  <AppInput
+                    placeholder="endereco@email.com"
+                    label="Endereço de e-mail"
+                    error={null}
+                    value={value}
+                    onChange={onChange}
+                  />
+                )}
+              />
+              <Controller
+                control={control}
+                name="phone"
+                defaultValue=""
+                render={({ field: { onChange, value } }) => (
+                  <AppInput
+                    placeholder="99-87766-5544"
+                    label="Seu número de telefone com DDD"
+                    error={null}
+                    value={value}
+                    onChange={onChange}
+                  />
+                )}
+              />
+            </Stack>
           </>
         )}
       </Flex>
