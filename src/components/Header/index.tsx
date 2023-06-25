f10089dae37405c6ee67c958906fc9c1a799a9ec
Thiago Bignotto <bignotto@gmail.com>
2023-06-25 07:08:20 -0300

    implemente user profile badge

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,17 @@
+import { api } from "@/services/api";
 import {
   Avatar,
+  AvatarBadge,
   Button,
   Flex,
   HStack,
-  IconButton,
   Text,
 } from "@chakra-ui/react";
+import { User } from "@prisma/client";
 import { signIn, signOut, useSession } from "next-auth/react";
-import { FormEvent } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import {
+  MdEdit,
   MdMessage,
   MdOutlineDashboard,
   MdOutlinePhonelinkErase,
@@ -17,6 +20,19 @@ import { RiLogoutBoxRLine } from "react-icons/ri";
 
 export default function Header() {
   const { data: session, status } = useSession();
+  const [userProfile, setUserProfile] = useState<User>();
+
+  useEffect(() => {
+    async function loadUserProfile() {
+      const response = await api.get(`users/${session?.user?.email}`);
+      setUserProfile(response.data);
+      console.log("loaded user again");
+    }
+
+    if (status === "authenticated") {
+      loadUserProfile();
+    }
+  }, [status]);
 
   async function handleLogin(event: FormEvent) {
     event.preventDefault();
@@ -35,7 +51,7 @@ export default function Header() {
       </Flex>
     );
 
-  if (status === "authenticated")
+  if (status === "authenticated") {
     return (
       <Flex w="100%" h="16" alignItems="center" justifyContent="space-between">
         <Flex>
@@ -43,7 +59,17 @@ export default function Header() {
             name={session.user?.name!}
             src={session.user?.image!}
             bg="teal.500"
-          />
+          >
+            {userProfile?.phoneConfirmed ? (
+              <AvatarBadge bg={"blue.500"} boxSize={"4"} borderWidth={"thin"}>
+                <MdEdit size={12} />
+              </AvatarBadge>
+            ) : (
+              <AvatarBadge bg={"red.500"} boxSize={"4"} borderWidth={"thin"}>
+                <MdEdit size={12} />
+              </AvatarBadge>
+            )}
+          </Avatar>
           <Flex flexDir={"column"} ml="2" justifyContent="center">
             <Text fontWeight="bold" fontSize="xs">
               {session.user?.name}
@@ -54,19 +80,16 @@ export default function Header() {
         <HStack>
           <MdOutlineDashboard size={24} />
           <MdMessage size={24} />
-          <Button variant={"unstyled"}>
+          <Button variant={"ghost"}>
             <MdOutlinePhonelinkErase size={24} />
           </Button>
-          <IconButton
-            aria-label="sign out button"
-            onClick={() => signOut()}
-            colorScheme="blue"
-            icon={<RiLogoutBoxRLine size={24} />}
-            variant={"unstyled"}
-          />
+          <Button onClick={() => signOut()} variant={"ghost"}>
+            <RiLogoutBoxRLine size={24} color="#2C5282" />
+          </Button>
         </HStack>
       </Flex>
     );
+  }
   return (
     <Flex w="100%" h="16" alignItems="center" justifyContent="space-between">
       <Avatar name={""} src={""} />
