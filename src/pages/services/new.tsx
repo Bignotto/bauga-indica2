548d2ac4a7eedae1cd74df7d8d6bf4870c3d0266
Thiago Bignotto <bignotto@gmail.com>
2023-07-06 06:49:46 -0300

    new service screen

--- src/pages/services/new.tsx
+++ src/pages/services/new.tsx
@@ -1,8 +1,15 @@
 import AppInput from "@/components/AppInput";
-import { HStack, Heading, Stack, Text } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Heading,
+  Stack,
+  Text,
+  Textarea,
+} from "@chakra-ui/react";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { MdInfo } from "react-icons/md";
 import * as yup from "yup";
@@ -16,9 +23,9 @@ type FormDataProps = {
 };
 
 const serviceSchema = yup.object({
-  title: yup.string(),
-  description: yup.string(),
-  value: yup.number(),
+  title: yup.string().required("O nome do serviço é obrigatório."),
+  description: yup.string().required("Uma descrição detalhada é obrigatório."),
+  value: yup.number().positive("Preço inválido"),
   serviceType: yup.number(),
 });
 
@@ -30,9 +37,20 @@ export default function NewService() {
     resolver: yupResolver(serviceSchema),
   });
 
+  const [serviceTypes, setServiceTypes] = useState();
+
   useEffect(() => {
     if (status === "unauthenticated") router.push("/");
   }, [router, status]);
+
+  async function handleSaveService({
+    title,
+    description,
+    serviceType,
+    value,
+  }: FormDataProps) {
+    console.log({ title, description, serviceType, value });
+  }
 
   return (
     <Stack alignItems="center" h="full" mt="4">
@@ -41,9 +59,9 @@ export default function NewService() {
         <Text fontWeight={"bold"} fontSize={"sm"}>
           Preencha os campos abaixo para criar um serviço.
         </Text>
-        <HStack bg="gray.500" px={"2"} color={"#FFFFFF"} my={"2"}>
+        <HStack bg="gray.500" p={"4"} color={"#FFFFFF"} my={"2"}>
           <MdInfo size={40} />
-          <Text fontSize={"sm"} color={"gray.100"}>
+          <Text fontSize={"sm"} color={"gray.100"} ml={"2"}>
             Um serviço é tudo aquilo que você pode fazer para o seu cliente.
           </Text>
         </HStack>
@@ -62,6 +80,47 @@ export default function NewService() {
             />
           )}
         />
+        <Controller
+          control={control}
+          name="description"
+          defaultValue=""
+          render={({ field: { onChange, value } }) => (
+            <AppInput
+              as={Textarea}
+              placeholder="descreva detalhes do serviço"
+              label="Descrição do serviço"
+              error={formState.errors.description?.message}
+              value={value}
+              onChange={onChange}
+              id="description"
+            />
+          )}
+        />
+        <Controller
+          control={control}
+          name="value"
+          render={({ field: { onChange, value } }) => (
+            <AppInput
+              placeholder="quanto custa"
+              label="Preço do serviço"
+              error={formState.errors.value?.message}
+              value={value}
+              onChange={onChange}
+              id="value"
+              inputMode="numeric"
+            />
+          )}
+        />
+        <HStack justifyContent={"center"}>
+          <Button w={"30%"}>Cancelar</Button>
+          <Button
+            w={"30%"}
+            colorScheme="blue"
+            onClick={handleSubmit(handleSaveService)}
+          >
+            Salvar
+          </Button>
+        </HStack>
       </Stack>
     </Stack>
   );
