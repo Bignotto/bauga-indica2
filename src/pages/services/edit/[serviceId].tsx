e630562cb2999a08b8850c7c6f0422bb8a4592c7
Thiago Bignotto <bignotto@gmail.com>
2023-07-14 07:44:20 -0300

    save commit

---
+++ src/pages/services/edit/[serviceId].tsx
@@ -0,0 +1,79 @@
+import Header from "@/components/Header";
+import { api } from "@/services/api";
+import {
+  Button,
+  Flex,
+  HStack,
+  Image,
+  Spinner,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { Service, ServiceType, User } from "@prisma/client";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { BsWhatsapp } from "react-icons/bs";
+
+export default function ServiceDetails() {
+  const router = useRouter();
+  const { serviceId } = router.query;
+
+  const [service, setService] = useState<
+    Service & {
+      provider: User;
+      serviceType: ServiceType;
+    }
+  >();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadService() {
+      try {
+        const response = await api.get(`/services/${serviceId}`);
+        setService(response.data);
+      } catch (error) {
+        console.log({ error });
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    if (serviceId) loadService();
+  }, [serviceId]);
+
+  return (
+    <Stack alignItems={"center"}>
+      <Flex h="full" px="4" flexDir="column" w={["100%", 500]}>
+        <Header />
+      </Flex>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <Stack h="full" px="4" w={["100%", 500]}>
+          <Text fontSize={"2xl"} fontWeight={"bold"} color={"black"}>
+            {service?.title}
+          </Text>
+          <Text>{service?.description}</Text>
+          <HStack>
+            <Image
+              src={service?.provider.image!}
+              alt="provider image"
+              boxSize={42}
+              rounded={"full"}
+            />
+            <Text fontWeight={"bold"} fontSize={"large"}>
+              {service?.provider.name}
+            </Text>
+          </HStack>
+          <Text fontSize={"large"} fontWeight={"bold"} color={"blue.500"}>
+            R$ {service?.value}
+          </Text>
+          <Button colorScheme="green" leftIcon={<BsWhatsapp size={20} />}>
+            Contato
+          </Button>
+        </Stack>
+      )}
+    </Stack>
+  );
+}
+
+//TODO: implement images list component
