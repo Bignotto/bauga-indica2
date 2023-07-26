e174243f1e18c0f84136d2d4597a86daa62b85ee
Thiago Bignotto <bignotto@gmail.com>
2023-07-26 06:55:11 -0300

    implemented stats page

--- src/pages/dashboard/clicks.tsx
+++ src/pages/dashboard/clicks.tsx
@@ -1,7 +1,6 @@
 import { api } from "@/services/api";
 import {
   Avatar,
-  Box,
   Flex,
   HStack,
   Heading,
@@ -12,6 +11,7 @@ import {
 import NextLink from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { MdBarChart } from "react-icons/md";
 import { useAuth } from "../../hooks/AuthContext";
 
 type ClickProps = {
@@ -41,7 +41,6 @@ export default function Clicks() {
     loadClicks();
   }, []);
 
-  //NEXT: show results to user
   return (
     <Stack alignItems="center" h="full" mt="4">
       <HStack
@@ -63,14 +62,6 @@ export default function Clicks() {
             </Text>
           </Stack>
         </Flex>
-        {/* <Button
-          colorScheme="blue"
-          size={"sm"}
-          leftIcon={<MdAdd size={25} />}
-          onClick={() => router.push("/services/new")}
-        >
-          Criar anúncio
-        </Button> */}
       </HStack>
       <Flex
         alignItems="center"
@@ -82,11 +73,39 @@ export default function Clicks() {
         <Heading>Clicks por serviço</Heading>
       </Flex>
       <Stack w={["100%", 500]} px={"4"}>
+        <HStack bg="gray.200" px="1" py="1">
+          <Flex w="16%" justifyContent={"flex-end"} px="1">
+            <Text fontSize={"smaller"} fontWeight={"bold"}>
+              Clicks
+            </Text>
+          </Flex>
+
+          <HStack
+            justifyContent={"space-between"}
+            alignItems={"center"}
+            w={"100%"}
+          >
+            <Text fontSize={"smaller"} fontWeight={"bold"}>
+              Serviço
+            </Text>
+          </HStack>
+        </HStack>
         {clicks.map((c) => (
-          <Box key={c.id} bg="gray.200">
-            <Text>{c.title}</Text>
-            <Text>{c.clicks}</Text>
-          </Box>
+          //TODO: create component
+          <HStack key={c.id} bg="gray.200" px="2">
+            <Flex w="16%" justifyContent={"center"} px="2">
+              <Text>{c.clicks}</Text>
+            </Flex>
+
+            <HStack
+              justifyContent={"space-between"}
+              alignItems={"center"}
+              w={"100%"}
+            >
+              <Text noOfLines={1}>{c.title}</Text>
+              <MdBarChart />
+            </HStack>
+          </HStack>
         ))}
       </Stack>
     </Stack>
