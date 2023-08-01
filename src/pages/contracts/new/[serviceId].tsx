216d7965ba802ee96bc58cfe5b59551c7dfccabb
Thiago Bignotto <bignotto@gmail.com>
2023-08-01 06:33:10 -0300

    created send provider message about a service page

--- src/pages/contracts/new/[serviceId].tsx
+++ src/pages/contracts/new/[serviceId].tsx
@@ -1,13 +1,38 @@
+import AppInput from "@/components/AppInput";
 import AppLogo from "@/components/AppLogo";
 import Header from "@/components/Header";
-import { Flex, Stack, Text } from "@chakra-ui/react";
+import { api } from "@/services/api";
+import { Button, Flex, Stack, Text, Textarea } from "@chakra-ui/react";
+import { Service, ServiceType, User } from "@prisma/client";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function NewContract() {
   const router = useRouter();
   const { serviceId } = router.query;
 
-  //NEXT: load service and create send message form
+  const [service, setService] = useState<
+    Service & {
+      provider: User;
+      serviceType: ServiceType;
+    }
+  >();
+  const [isLoading, setIsLoading] = useState(true);
+
+  //NEXT: create new contract into database and create a contract-message page
+  useEffect(() => {
+    async function loadService() {
+      try {
+        const response = await api.get(`/services/${serviceId}`);
+        setService(response.data);
+      } catch (error) {
+        console.log({ error });
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    if (serviceId) loadService();
+  }, [serviceId]);
 
   return (
     <Stack alignItems={"center"}>
@@ -16,7 +41,21 @@ export default function NewContract() {
         <Flex alignItems="center" justifyContent="space-between" my="4">
           <AppLogo size="md" />
         </Flex>
-        <Text>New contract for {serviceId}</Text>
+        <Text fontSize={"sm"}>
+          Contatar <strong>{service?.provider.name}</strong> sobre
+        </Text>
+        <Text fontSize={"lg"} fontWeight={"bold"}>
+          {service?.title}
+        </Text>
+        <Stack mt="2">
+          <AppInput
+            as={Textarea}
+            error=""
+            label="Mensagem"
+            resize={"vertical"}
+          />
+          <Button colorScheme="green">Enviar mensagem</Button>
+        </Stack>
       </Flex>
     </Stack>
   );
