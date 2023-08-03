e2e017116e32450e63d4a637c839d9bab7780453
Thiago Bignotto <bignotto@gmail.com>
2023-08-03 06:55:13 -0300

    implemented contract messages page

--- src/pages/contracts/[contractId].tsx
+++ src/pages/contracts/[contractId].tsx
@@ -1,7 +1,8 @@
+import AppInput from "@/components/AppInput";
 import AppLogo from "@/components/AppLogo";
 import Header from "@/components/Header";
 import { api } from "@/services/api";
-import { Flex, Stack, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Stack, Text, Textarea } from "@chakra-ui/react";
 import { Contract, Message, Service, User } from "@prisma/client";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
@@ -22,6 +23,8 @@ export default function ContractMessages() {
     }
   >();
 
+  const [message, setMessage] = useState("");
+
   useEffect(() => {
     async function loadContract() {
       try {
@@ -35,7 +38,8 @@ export default function ContractMessages() {
     if (contractId) loadContract();
   }, [contractId, router, status]);
 
-  //NEXT: style this page
+  //NEXT: handle add new message
+
   return (
     <Stack alignItems={"center"}>
       <Flex p={"4"} flexDir={"column"} w={["100%", 500]}>
@@ -43,12 +47,61 @@ export default function ContractMessages() {
         <Flex alignItems="center" justifyContent="space-between" my="4">
           <AppLogo size="sm" />
         </Flex>
-        <Text>This is contract page for contract number {contractId}</Text>
-        {contract?.messages.map((m) => (
-          <Flex bg="gray.200" key={m.id}>
-            <Text>{m.text}</Text>
+
+        <Box bg="gray.200" p="1">
+          <Text
+            fontSize={"md"}
+            fontWeight={"bold"}
+            bg={"green.600"}
+            color={"white"}
+            px="1"
+          >
+            {contract?.service?.title}
+          </Text>
+          <Text fontSize={"sm"} p="2">
+            {contract?.service?.description}
+          </Text>
+          <Flex justifyContent={"flex-end"}>
+            <Text fontSize={"lg"} color={"blue.400"} fontWeight={"bold"}>
+              R$ {contract?.service?.value}
+            </Text>
           </Flex>
-        ))}
+        </Box>
+
+        <Stack mt={"2"}>
+          {contract?.messages.map((m) => (
+            <Flex
+              key={m.id}
+              flexDir={"row"}
+              justifyContent={
+                session?.userId === m.userFromId ? "flex-end" : "flex-start"
+              }
+            >
+              <Flex
+                bg={session?.userId === m.userFromId ? "green.100" : "gray.200"}
+                w={"80%"}
+              >
+                <Text fontSize={"sm"} p={"2"}>
+                  {m.text}
+                </Text>
+              </Flex>
+            </Flex>
+          ))}
+          <Stack mt="2">
+            <AppInput
+              as={Textarea}
+              error=""
+              label="Mensagem"
+              resize={"vertical"}
+              fontSize={"sm"}
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <Button colorScheme="green" isDisabled={message.length === 0}>
+              Enviar mensagem
+            </Button>
+          </Stack>
+        </Stack>
       </Flex>
     </Stack>
   );
