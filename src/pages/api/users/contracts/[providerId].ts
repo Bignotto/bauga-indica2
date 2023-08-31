74309516342171b3b3000fcbd00363db0d4723d1
Thiago Bignotto <bignotto@gmail.com>
2023-08-31 06:06:05 -0300

    added user contractor to return array when listing users contracts

--- src/pages/api/users/contracts/[providerId].ts
+++ src/pages/api/users/contracts/[providerId].ts
@@ -16,6 +16,7 @@ export default async function handler(
       },
       include: {
         service: true,
+        userContractor: true,
       },
     });
 
--- src/pages/contracts/index.tsx
+++ src/pages/contracts/index.tsx
@@ -1,8 +1,30 @@
 import AppLogo from "@/components/AppLogo";
 import Header from "@/components/Header";
+import { api } from "@/services/api";
 import { Flex, Heading, Stack } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useAuth } from "../../hooks/AuthContext";
 
 export default function Contracts() {
+  const router = useRouter();
+  const { status, session } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadDashboardProps() {
+      try {
+        const response = await api.get("users/dashboard");
+      } catch (error) {
+        console.log({ error });
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    if (status === "unauthenticated") router.push("/");
+    if (status === "authenticated") loadDashboardProps();
+  }, [router, status]);
+
   return (
     <Stack alignItems={"center"}>
       <Flex p={"4"} flexDir={"column"} w={["100%", 500]}>
